af55bf7eef093947367bb2fe05428480cf073c28
Saad Umair <saad.ansari0106@gmail.com>
2024-08-18 12:55:53 +0530

    Fix I/O path

--- Bit-Magic/3-TotalSetBits.cpp
+++ Bit-Magic/3-TotalSetBits.cpp
@@ -27,8 +27,8 @@ int main()
     // ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
     // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
     // #endif
 
     int input;
--- Bit-Magic/6-BitDifference.cpp
+++ Bit-Magic/6-BitDifference.cpp
@@ -21,8 +21,8 @@ int main()
     // ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
     // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    // freopen("../input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout);
     // #endif
 
     ll a, b;
